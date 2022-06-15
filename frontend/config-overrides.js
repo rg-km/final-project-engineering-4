9354119de16b5a487cb4b1eac2111280034457ce
Rayhan <rayhanprima99@gmail.com>
2022-06-15 11:03:57 +0700

    chore (env): update eslint, update folder structure, and add alias mechanism

---
+++ frontend/config-overrides.js
@@ -0,0 +1,15 @@
+const { alias } = require('react-app-rewire-alias');
+
+module.exports = function override(config) {
+  alias({
+    '@images': 'src/images',
+    '@styles': './src/styles',
+    '@routes': './src/routes',
+    '@services': './src/services',
+    '@store': './src/store',
+    '@utils': './src/utils',
+    '@tests': './src/tests',
+  })(config);
+
+  return config;
+};
