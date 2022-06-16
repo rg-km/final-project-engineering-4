2b3665b0548369c46428054940a9c5f89c3e9092
rikzan fernanda <fernandarikzan@gmail.com>
2022-06-16 10:29:51 +0700

    chore (FE): create register page for teacher

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -2,11 +2,11 @@ import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import RoleItem from 'src/components/RoleItem';
-import bg_auth from "@images/bg_auth.jpg";
 import teacher from "@images/teaching.png";
+import BgAuth from 'src/components/BgAuth';
 
 const Register = () => {
-    return <Box bg={'gray.50'} h={'100vh'} display={'flex'} bgImg={bg_auth} bgRepeat={'no-repeat'} bgSize={'cover'}>
+    return <BgAuth>
         <Box bg={'white'} w={['90%', '50%']} m={'auto'} p={[4, 6, 10, 20]} borderRadius={['xl', '2xl']} boxShadow={['2xl']}>
             <Heading textAlign={'center'} fontSize={['sm', 'md', 'lg']} mb={[3, 4, 8]}>Daftar sebagai</Heading>
             <Flex gap={[2, 4, 8]} justifyContent={'center'} mb={[2, 4]}>
@@ -14,9 +14,9 @@ const Register = () => {
                 <RoleItem title={'Orang Tua'} redirect={'/register/parent'} img={teacher} bg={'blue.200'} />
                 <RoleItem title={'Siswa'} redirect={'/register/student'} img={teacher} bg={'green.200'} />
             </Flex>
-            <Text textAlign={'center'} fontSize={['xs', 'sm']}>Atau <Link to={'/login'} style={{ textDecoration: 'underline' }}>masuk</Link></Text>
+            <Text textAlign={'center'} fontSize={['xs', 'sm']}>Atau <Link to={'/'} style={{ textDecoration: 'underline' }}>masuk</Link></Text>
         </Box>
-    </Box>
+    </BgAuth>
 }
 
 export default Register;
--- frontend/src/routes/App.jsx
+++ frontend/src/routes/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "src/pages/auth/Register";
+import RegisterTeacher from "src/pages/auth/RegisterTeacher";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/register/teacher" element={<RegisterTeacher />} />
       </Routes>
     </BrowserRouter>
   );
