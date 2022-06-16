cbe6adf01ec250817ac639905e22a0415307a4f4
rikzan fernanda <fernandarikzan@gmail.com>
2022-06-16 10:59:06 +0700

    chore (FE): create registration page for parent and student

--- frontend/src/pages/auth/RegisterParent.jsx
+++ frontend/src/pages/auth/RegisterParent.jsx
@@ -5,14 +5,12 @@ import BgAuth from 'src/components/BgAuth';
 import InputPassword from 'src/components/InputPassword';
 import InputText from 'src/components/InputText';
 
-const RegisterTeacher = () => {
+const RegisterParent = () => {
     const initilInput = {
         nama: "",
         username: "",
         email: "",
         jenis_kelamin: "",
-        pendidikan: "",
-        agama: "",
         no_hp: "",
         alamat: "",
         password: ""
@@ -36,19 +34,17 @@ const RegisterTeacher = () => {
 
     return <BgAuth>
         <Box overflow={'auto'} bg={'white'} maxH={'80vh'} w={['90%', '50%']} m={'auto'} p={[4, 6, 10, 20]} borderRadius={['xl', '2xl']} boxShadow={['2xl']}>
-            <Heading textAlign={'center'} fontSize={['sm', 'md', 'lg']} mb={[3, 4, 8]}>Daftar sebagai Guru</Heading>
+            <Heading textAlign={'center'} fontSize={['sm', 'md', 'lg']} mb={[3, 4, 8]}>Daftar sebagai Orang Tua</Heading>
             <form>
                 <InputText name='nama' label='Nama' value={input.nama} handleChange={handleChange} isRequired />
                 <InputText name='username' label='Username' value={input.username} handleChange={handleChange} isRequired />
                 <InputText name='email' label='Email' value={input.email} handleChange={handleChange} isRequired />
                 <InputText name='jenis_kelamin' label='Jenis Kelamin' value={input.jenis_kelamin} handleChange={handleChange} isRequired />
-                <InputText name='pendidikan' label='Pendidikan' value={input.pendidikan} handleChange={handleChange} />
-                <InputText name='agama' label='Agama' value={input.agama} handleChange={handleChange} />
                 <InputText name='no_hp' label='No HP' value={input.no_hp} handleChange={handleChange} isRequired />
                 <InputText name='alamat' label='Alamat' value={input.alamat} handleChange={handleChange} />
                 <InputPassword name='password' label='Password' value={input.password} handleChange={handleChange} isRequired />
 
-                <Flex justify={'center'} gap={[2]} mb={[2,4]}>
+                <Flex justify={'center'} gap={[2]} mb={[2, 4]}>
                     <Link to={'/register'}><Button size={['sm', 'md']}>Kembali</Button></Link>
                     <Button type='submit' isLoading={isLoading} onClick={onSubmit} colorScheme='blue' size={['sm', 'md']}>Daftar</Button>
                 </Flex>
@@ -59,4 +55,4 @@ const RegisterTeacher = () => {
     </BgAuth>
 }
 
-export default RegisterTeacher;
+export default RegisterParent;
--- frontend/src/routes/App.jsx
+++ frontend/src/routes/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "src/pages/auth/Register";
+import RegisterParent from "src/pages/auth/RegisterParent";
+import RegisterStudent from "src/pages/auth/RegisterStudent";
 import RegisterTeacher from "src/pages/auth/RegisterTeacher";
 
 const App = () => {
@@ -8,6 +10,8 @@ const App = () => {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/register/teacher" element={<RegisterTeacher />} />
+        <Route path="/register/parent" element={<RegisterParent />} />
+        <Route path="/register/student" element={<RegisterStudent />} />
       </Routes>
     </BrowserRouter>
   );
