ad473d023c3f2f5f2efad60aa119cd4248043a3c
Rayhan <rayhanprima99@gmail.com>
2022-06-21 20:08:09 +0700

    chore: add config folder as alias

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -3,6 +3,7 @@ const { alias } = require('react-app-rewire-alias');
 module.exports = function override(config) {
   alias({
     '@images': './src/images',
+    '@config': './src/config',
     '@styles': './src/styles',
     '@routes': './src/routes',
     '@services': './src/services',
