a16ce2bbc0fed98f7c7ff949fa33847ef83814b6
Rayhan <rayhanprima99@gmail.com>
2022-06-22 04:08:17 +0700

    chore (InputText): add meta props, error message & helper message

--- frontend/src/components/InputText.jsx
+++ frontend/src/components/InputText.jsx
@@ -1,11 +1,14 @@
-import { FormControl, FormLabel, Input } from '@chakra-ui/react';
-import React from 'react';
+import { FormControl, FormLabel, Input, FormErrorMessage, FormHelperText } from '@chakra-ui/react';
 
-const InputText = ({ type, value, name, label, onChange, required }) => {
+const InputText = ({ type, value, name, label, onChange, meta, required, helperMessage, errorMessage }) => {
   return (
     <FormControl isRequired={required}>
       <FormLabel>{label}</FormLabel>
       <Input type={type} name={name} value={value} onChange={onChange} bg={'white'} required={required} />
+      {!meta && !errorMessage && helperMessage && <FormHelperText>{helperMessage}</FormHelperText>}
+      {((meta && meta.touched && meta.error) || errorMessage) && (
+        <FormErrorMessage>{errorMessage}</FormErrorMessage>
+      )}
     </FormControl>
   );
 };
