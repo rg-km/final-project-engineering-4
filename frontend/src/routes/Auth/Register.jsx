add01e7e8b4e0eea1bba7d2d6260605890172fd2
Rayhan <rayhanprima99@gmail.com>
2022-06-22 03:56:08 +0700

    chore: adjust updated input props to register

--- frontend/src/routes/Auth/Register.jsx
+++ frontend/src/routes/Auth/Register.jsx
@@ -1,80 +1,70 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Stack, Text } from '@chakra-ui/react';
+import { Formik, Form } from 'formik';
 import { PATH } from '@routes/path';
 import AuthContainer from './components/AuthContainer';
 import InputText from '@components/InputText';
 import InputPassword from '@components/InputPassword';
 
+const INITIAL_VALUES = {
+  nama: '',
+  username: '',
+  email: '',
+  jenis_kelamin: '',
+  pendidikan: '',
+  agama: '',
+  no_hp: '',
+  alamat: '',
+  password: '',
+  confirm_password: '',
+};
+
 const Register = ({ role }) => {
-  const initilInput = {
-    nama: '',
-    username: '',
-    email: '',
-    jenis_kelamin: '',
-    pendidikan: '',
-    agama: '',
-    no_hp: '',
-    alamat: '',
-    password: '',
-  };
-
-  const [isLoading, setIsLoading] = useState(false);
-  const [input, setInput] = useState(initilInput);
-
-  const handleChange = (e) => {
-    setInput({
-      ...input,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-    console.log(input);
-    setIsLoading(false);
+  const handleSubmit = (values) => {
+    console.log({ values });
   };
 
   return (
     <AuthContainer
       title={`Daftar sebagai ${role.title}`}
       subtitle="Lengkapi form di bawah dengan menggunakan data Anda yang valid">
-      <Stack as={'form'} onSubmit={onSubmit} spacing={8}>
-        <Stack spacing={3}>
-          <InputText name="nama" label="Nama" value={input.nama} handleChange={handleChange} isRequired />
-          <InputText
-            name="username"
-            label="Username"
-            value={input.username}
-            handleChange={handleChange}
-            isRequired
-          />
-          <InputText name="email" label="Email" value={input.email} handleChange={handleChange} isRequired />
-          <InputText
-            name="jenis_kelamin"
-            label="Jenis Kelamin"
-            value={input.jenis_kelamin}
-            handleChange={handleChange}
-            isRequired
-          />
-          <InputText name="pendidikan" label="Pendidikan" value={input.pendidikan} handleChange={handleChange} />
-          <InputText name="agama" label="Agama" value={input.agama} handleChange={handleChange} />
-          <InputText name="no_hp" label="No HP" value={input.no_hp} handleChange={handleChange} isRequired />
-          <InputText name="alamat" label="Alamat" value={input.alamat} handleChange={handleChange} />
-          <InputPassword
-            name="password"
-            label="Password"
-            value={input.password}
-            handleChange={handleChange}
-            isRequired
-          />
-        </Stack>
+      <Formik initialValues={INITIAL_VALUES} onSubmit={handleSubmit}>
+        {({ values, handleChange }) => (
+          <Form>
+            <Stack spacing={8}>
+              <Stack spacing={3}>
+                <InputText name="nama" label="Nama" value={values.nama} onChange={handleChange} />
+                <InputText name="username" label="Username" value={values.username} onChange={handleChange} />
+                <InputText name="email" label="Email" value={values.email} onChange={handleChange} />
+                <InputText
+                  name="jenis_kelamin"
+                  label="Jenis Kelamin"
+                  value={values.jenis_kelamin}
+                  onChange={handleChange}
+                />
+                <InputText
+                  name="pendidikan"
+                  label="Pendidikan"
+                  value={values.pendidikan}
+                  onChange={handleChange}
+                />
+                <InputText name="agama" label="Agama" value={values.agama} onChange={handleChange} />
+                <InputText name="no_hp" label="No HP" value={values.no_hp} onChange={handleChange} />
+                <InputText name="alamat" label="Alamat" value={values.alamat} onChange={handleChange} />
+                <InputPassword name="password" label="Password" value={values.password} onChange={handleChange} />
+                <InputPassword
+                  name="confirm_password"
+                  label="Konfirmasi Password"
+                  value={values.confirm_password}
+                  onChange={handleChange}
+                />
+              </Stack>
 
-        <Button type="submit" isLoading={isLoading}>
-          Daftar
-        </Button>
-      </Stack>
+              <Button type="submit">Daftar</Button>
+            </Stack>
+          </Form>
+        )}
+      </Formik>
 
       <Stack>
         <Text textAlign={'center'} fontSize={'sm'}>
