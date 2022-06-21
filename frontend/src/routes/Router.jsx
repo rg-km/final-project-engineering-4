651b599b8d2c41f8bcee286b572ead4e97a42d48
Rayhan <rayhanprima99@gmail.com>
2022-06-22 01:07:16 +0700

    chore (Router): add login for parent & student role

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -12,6 +12,8 @@ function Router() {
         <Route exact path={PATH.LOGIN} element={<RoleSelection />} />
         <Route exact path={PATH.REGISTER} element={<Navigate to={PATH.LOGIN} />} />
         <Route path={`${PATH.LOGIN}/${USER_ROLES.TEACHER.value}`} element={<Login role={USER_ROLES.TEACHER} />} />
+        <Route path={`${PATH.LOGIN}/${USER_ROLES.PARENT.value}`} element={<Login role={USER_ROLES.PARENT} />} />
+        <Route path={`${PATH.LOGIN}/${USER_ROLES.STUDENT.value}`} element={<Login role={USER_ROLES.STUDENT} />} />
       </Routes>
     </div>
   );
