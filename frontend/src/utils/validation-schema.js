94e722a9f77d9f04db8b7e4a0b66265a71e6bfec
Rayhan <rayhanprima99@gmail.com>
2022-06-22 11:27:22 +0700

    chore (validation schema): remove password minimum

--- frontend/src/utils/validation-schema.js
+++ frontend/src/utils/validation-schema.js
@@ -9,11 +9,16 @@ const registerSchema = yup.object().shape({
     .matches(REGEX_PATTERN.number, 'No HP tidak valid')
     .min(10, 'No HP tidak valid')
     .required('No HP harus diisi'),
-  password: yup.string().min(8, 'Password minimal 8 karakter').required('Password harus diisi'),
+  password: yup.string().required('Password harus diisi'),
   confirm_password: yup
     .string()
     .oneOf([yup.ref('password'), null], 'Password tidak sama')
     .required('Konfirmasi password harus diisi'),
 });
 
-export const VALIDATION_SCHEMA = { registerSchema };
+const loginSchema = yup.object().shape({
+  email: yup.string().email('Email tidak valid').required('Email harus diisi'),
+  password: yup.string().required('Password harus diisi'),
+});
+
+export const VALIDATION_SCHEMA = { registerSchema, loginSchema };
