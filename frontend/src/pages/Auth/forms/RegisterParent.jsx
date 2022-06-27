7e016059a2ff7d930f4f0826bdf7d90d13752e43
rikzan fernanda <fernandarikzan@gmail.com>
2022-06-27 22:26:34 +0700

    chore (FE): handle register parent and validation schema

--- frontend/src/pages/Auth/forms/RegisterParent.jsx
+++ frontend/src/pages/Auth/forms/RegisterParent.jsx
@@ -1,67 +1,130 @@
-import { Button, Flex } from '@chakra-ui/react';
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Button, Stack, useToast } from '@chakra-ui/react';
+import { Form, Formik } from 'formik';
+import React from 'react';
 import InputPassword from 'src/components/InputPassword';
 import InputText from 'src/components/InputText';
+import { USER_ROLES } from '@utils/constants';
+import { useNavigate } from 'react-router-dom';
+import { useAuthStore } from '@store/auth.store';
+import { VALIDATION_SCHEMA } from '@utils/validation-schema';
+
+const INITIAL_VALUES = {
+  nama: '',
+  email: '',
+  jenis_kelamin: '',
+  no_hp: '',
+  alamat: '',
+  email_siswa: '',
+  password: '',
+  confirm_password: '',
+};
 
 const RegisterParent = () => {
-  const initilInput = {
-    nama: '',
-    username: '',
-    email: '',
-    jenis_kelamin: '',
-    no_hp: '',
-    alamat: '',
-    password: '',
-  };
-  const [isLoading, setIsLoading] = useState(false);
-  const [input, setInput] = useState(initilInput);
+  const toast = useToast();
+  const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    setInput({
-      ...input,
-      [e.target.name]: e.target.value,
+  const { isLoading } = useAuthStore((state) => state); // data
+  const { handleRegister } = useAuthStore((state) => state); // action
+
+  const handleSubmit = ({
+    nama,
+    email,
+    jenis_kelamin,
+    no_hp,
+    alamat,
+    email_siswa,
+    password,
+    confirm_password,
+  }) => {
+    const data = { nama, email, jenis_kelamin, no_hp, alamat, email_siswa, password, confirm_password };
+
+    handleRegister(USER_ROLES.PARENT.value, data, (success, message) => {
+      toast({
+        status: success ? 'success' : 'error',
+        position: 'top-right',
+        title: message,
+        duration: 2500,
+        isClosable: true,
+      });
+
+      navigate(PATH.LOGIN);
     });
   };
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-    console.log(input);
-    setIsLoading(false);
-  };
-
   return (
-    <form onSubmit={onSubmit}>
-      <InputText name="nama" label="Nama" value={input.nama} handleChange={handleChange} isRequired />
-      <InputText name="username" label="Username" value={input.username} handleChange={handleChange} isRequired />
-      <InputText name="email" label="Email" value={input.email} handleChange={handleChange} isRequired />
-      <InputText
-        name="jenis_kelamin"
-        label="Jenis Kelamin"
-        value={input.jenis_kelamin}
-        handleChange={handleChange}
-        isRequired
-      />
-      <InputText name="no_hp" label="No HP" value={input.no_hp} handleChange={handleChange} isRequired />
-      <InputText name="alamat" label="Alamat" value={input.alamat} handleChange={handleChange} />
-      <InputPassword
-        name="password"
-        label="Password"
-        value={input.password}
-        handleChange={handleChange}
-        isRequired
-      />
+    <Formik
+      initialValues={INITIAL_VALUES}
+      onSubmit={handleSubmit}
+      validationSchema={VALIDATION_SCHEMA.registerParentSchema}>
+      {({ values, handleSubmit, getFieldMeta, handleChange }) => (
+        <Form onSubmit={handleSubmit}>
+          <Stack spacing={8}>
+            <Stack spacing={3}>
+              <InputText
+                name="nama"
+                label="Nama"
+                value={values.nama}
+                meta={getFieldMeta('nama')}
+                onChange={handleChange}
+              />
+              <InputText
+                name="email"
+                label="Email"
+                value={values.email}
+                meta={getFieldMeta('email')}
+                onChange={handleChange}
+              />
+              <InputText
+                name="jenis_kelamin"
+                label="Jenis Kelamin"
+                meta={getFieldMeta('jenis_kelamin')}
+                value={values.jenis_kelamin}
+                onChange={handleChange}
+              />
+              <InputText
+                name="no_hp"
+                label="No HP"
+                value={values.no_hp}
+                meta={getFieldMeta('no_hp')}
+                onChange={handleChange}
+              />
+              <InputText
+                name="alamat"
+                label="Alamat"
+                value={values.alamat}
+                meta={getFieldMeta('alamat')}
+                onChange={handleChange}
+              />
+              <InputText
+                name="email_siswa"
+                label="Email Siswa"
+                value={values.email_siswa}
+                meta={getFieldMeta('email_siswa')}
+                onChange={handleChange}
+              />
+              <InputPassword
+                name="password"
+                label="Password"
+                value={values.password}
+                meta={getFieldMeta('password')}
+                onChange={handleChange}
+              />
+              <InputPassword
+                name="confirm_password"
+                label="Konfirmasi Password"
+                value={values.confirm_password}
+                meta={getFieldMeta('confirm_password')}
+                onChange={handleChange}
+              />
+            </Stack>
 
-      <Flex justify={'center'} gap={[2]} mb={[2, 4]}>
-        <Link to={'/register'}>
-          <Button size={['sm', 'md']}>Kembali</Button>
-        </Link>
-        <Button type="submit" isLoading={isLoading} colorScheme="blue" size={['sm', 'md']}>
-          Daftar
-        </Button>
-      </Flex>
-    </form>
+            <Button type="submit" isLoading={isLoading}>
+              Daftar
+            </Button>
+          </Stack>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
