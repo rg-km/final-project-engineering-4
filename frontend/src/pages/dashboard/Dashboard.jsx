b52525d820817a6fe3dbb5134b40cd7aa6511882
Rayhan <rayhanprima99@gmail.com>
2022-06-27 19:04:25 +0700

    chore: simplify mock dashboard

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,82 +1,105 @@
-import { Box, Button, Heading, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, SimpleGrid, useDisclosure } from "@chakra-ui/react";
-import InputText from "@components/InputText";
-import { PATH } from "@config/path";
-import { useEffect, useState } from "react";
-import AddClass from "./components/AddClass";
-import ClassItem from "./components/ClassItem";
-import Navbar from "./components/Navbar";
+import {
+  Box,
+  Button,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  SimpleGrid,
+  useDisclosure,
+} from '@chakra-ui/react';
+import InputText from '@components/InputText';
+import { PATH } from '@config/path';
+import { useState } from 'react';
+import AddClass from './components/AddClass';
+import ClassItem from './components/ClassItem';
+import Navbar from './components/Navbar';
+
+const DATA = [
+  {
+    id_class: 1,
+    nama_kelas: '9F',
+    kode_kelas: 'qwertyasdf',
+    keterangan: 'Kelas dengan wali kelas yang bernama Bu Nurul Huda',
+  },
+  {
+    id_class: 2,
+    nama_kelas: '9E',
+    kode_kelas: 'qwertyuiop',
+    keterangan: 'Kelas dengan wali kelas yang bernama Bu Sum',
+  },
+  {
+    id_class: 3,
+    nama_kelas: '9D',
+    kode_kelas: 'zxcvasdf',
+    keterangan: 'Kelas dengan wali kelas yang bernama Pak Rizky',
+  },
+];
 
 const initialInput = {
-    nama_kelas: '',
-    keterangan: ''
-}
+  nama_kelas: '',
+  keterangan: '',
+};
 
 const Dashboard = () => {
-    let data = [
-        {
-            id_class: 1,
-            nama_kelas: "9F",
-            kode_kelas: "qwertyasdf",
-            keterangan: "Kelas dengan wali kelas yang bernama Bu Nurul Huda",
-        },
-        {
-            id_class: 2,
-            nama_kelas: "9E",
-            kode_kelas: "qwertyuiop",
-            keterangan: "Kelas dengan wali kelas yang bernama Bu Sum",
-        },
-        {
-            id_class: 3,
-            nama_kelas: "9D",
-            kode_kelas: "zxcvasdf",
-            keterangan: "Kelas dengan wali kelas yang bernama Pak Rizky",
-        }
-    ];
+  const studentClasses = DATA;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [input, setInput] = useState(initialInput);
 
-    const [classes, setClasses] = useState([]);
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const [input, setInput] = useState(initialInput);
+  const handleSubmit = () => {
+    console.log(input);
+  };
 
-    useEffect(() => {
-        setClasses(data);
-    }, [])
+  return (
+    <Box bg={'gray.100'} minH={'100vh'}>
+      <Navbar />
+      <Box px={{ base: 2, md: 20 }} py={{ base: 4, md: 8 }}>
+        <Heading fontSize={{ base: 'xs', md: 'md' }} mb={{ base: 2 }}>
+          Daftar Kelas Anda
+        </Heading>
 
-    const handleSubmit = () => {
-        console.log(input);
-    }
+        <SimpleGrid columns={{ base: 2, md: 4 }} spacing={{ base: 2, md: 4 }}>
+          <AddClass to={PATH.DASHBOARD} onClick={onOpen} />
+          {studentClasses.length > 0 &&
+            studentClasses.map((kelas, index) => {
+              return <ClassItem key={index} to={PATH.DASHBOARD} kelas={kelas} />;
+            })}
+        </SimpleGrid>
 
-    return <Box bg={'gray.100'} minH={'100vh'}>
-        <Navbar />
-        <Box px={{ "base": 2, "md": 20 }} py={{ "base": 4, "md": 8 }}>
-            <Heading fontSize={{ "base": "xs", "md": "md" }} mb={{ "base": 2 }}>Daftar Kelas Anda</Heading>
-
-            <SimpleGrid columns={{ "base": 2, "md": 4 }} spacing={{ "base": 2, md: 4 }}>
-                <AddClass to={PATH.DASHBOARD} onClick={onOpen} />
-                {
-                    classes.length > 0 && classes.map((kelas, index) => {
-                        return <ClassItem key={index} to={PATH.DASHBOARD} kelas={kelas} />
-                    })
-                }
-            </SimpleGrid>
-
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader fontSize={{base: 'md'}}>Tambah Kelas Baru</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <InputText type={'text'} label={'Nama Kelas'} value={input.nama_kelas} onChange={(e) => setInput({ ...input, nama_kelas: e.target.value })} required />
-                        <InputText type={'text'} label={'Keterangan'} value={input.keterangan} onChange={(e) => setInput({ ...input, keterangan: e.target.value })} />
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button colorScheme='blue' onClick={handleSubmit} size={{base: 'sm'}}>
-                            Tambah Baru
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </Box>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader fontSize={{ base: 'md' }}>Tambah Kelas Baru</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <InputText
+                type={'text'}
+                label={'Nama Kelas'}
+                value={input.nama_kelas}
+                onChange={(e) => setInput({ ...input, nama_kelas: e.target.value })}
+                required
+              />
+              <InputText
+                type={'text'}
+                label={'Keterangan'}
+                value={input.keterangan}
+                onChange={(e) => setInput({ ...input, keterangan: e.target.value })}
+              />
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" onClick={handleSubmit} size={{ base: 'sm' }}>
+                Tambah Baru
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Box>
     </Box>
-}
+  );
+};
 
 export default Dashboard;
