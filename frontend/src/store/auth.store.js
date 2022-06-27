759b935cb96e4a8af192ab2b4ca01f836fa642ef
Rayhan <rayhanprima99@gmail.com>
2022-06-27 21:39:07 +0700

    chore (auth store): add role state when logged in

--- frontend/src/store/auth.store.js
+++ frontend/src/store/auth.store.js
@@ -8,6 +8,7 @@ const useAuthStore = create(
       (set) => ({
         token: '',
         user: null,
+        role: null,
         isLoading: false,
         handleLogin: async (role, email, password, callback) => {
           set({ isLoading: true });
@@ -17,6 +18,7 @@ const useAuthStore = create(
           set({
             token: response.success ? response.payload.token : null,
             user: response.success ? response.payload.user : null,
+            role,
             isLoading: false,
           });
 
